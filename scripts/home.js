2b8cd3902e64ce7f80f4689bdd9285a284983032
Addi-11 <saddi.5131@gmail.com>
2019-07-12 11:34:08 +0530

    slideshow update

--- scripts/home.js
+++ scripts/home.js
@@ -1,28 +1,30 @@
 var images = [
-	'resources/images/iste-team.jpeg',
 	'resources/images/iste-ocean.jpeg',
+	'resources/images/iste-team.jpeg',
 	'resources/images/iste-tree.jpeg',
 	'resources/images/iste-time.jpeg',
 	'resources/images/iste-cat.jpeg'
 ];
 
-var imgTime = 3000; //transition time for one image
+var imgTime = 5000; //transition time for one image
 var i = 0;//current image index
-
+var bg = document.querySelectorAll('#slideshow')
 function changeBg(){
-	//setting the initial image
-	document.getElementById('slideshow').className += "fadeOut";
 
-	//switching between fadeOut class
-	setTimeout(function(){
-		document.getElementById('slideshow').src = images[i];
-		document.getElementById('slideshow').className = "";
-	}, 1000);
+
+	//bg[0].classList.remove('visible');
+	//bg[1].classList.remove('invisible');
+	bg[0].src = images[i];
+	bg[1].src = images[i+1];
+
+	bg[0].classList.toggle('visible');
+	bg[1].classList.toggle('invisible');
 	i++;
 	//resetting image index
-	if(i == images.length){i = 0;}
+	if(i + 1 == images.length){i = 0;}
 
 	setTimeout(changeBg, imgTime);
+
 };
 
 changeBg();
